cd05c49d89cba2f5079062d65ddad956049d14f2
retore <44674058+retore404@users.noreply.github.com>
2020-08-03 22:12:23 +0900

    update: ユーザーをDjango提供のUserクラスを使用するように変更

--- logger/models.py
+++ logger/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # ボス情報を管理するモデル
 class Boss(models.Model):
@@ -7,6 +8,6 @@ class Boss(models.Model):
 
 # 各ユーザーのボス挑戦履歴を管理するモデル
 class UserBossHistory(models.Model):
-    user_id = models.CharField(max_length=3)
+    user_id = models.ForeignKey(User, to_field='username', on_delete=models.PROTECT)
     boss_id = models.ForeignKey(Boss, to_field='boss_id', on_delete=models.PROTECT)
     last_challenged_date = models.DateTimeField('date challenged')
