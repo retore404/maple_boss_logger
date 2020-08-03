ff5197b9354406e61d0ccfc6dc804972fee12d9f
retore <44674058+retore404@users.noreply.github.com>
2020-08-03 12:13:09 +0900

    構想中のモデルを作成

--- logger/admin.py
+++ logger/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Boss
+from .models import UserBossHistory
+
+admin.site.register(Boss)
+admin.site.register(UserBossHistory)
--- logger/models.py
+++ logger/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# ボス情報を管理するモデル
+class Boss(models.Model):
+    boss_id = models.CharField(max_length=3, unique=True)
+    boss_name = models.CharField(max_length=60)
+
+# 各ユーザーのボス挑戦履歴を管理するモデル
+class UserBossHistory(models.Model):
+    user_id = models.CharField(max_length=3)
+    boss_id = models.ForeignKey(Boss, to_field='boss_id', on_delete=models.PROTECT)
+    last_challenged_date = models.DateTimeField('date challenged')
