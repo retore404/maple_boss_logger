337f26fd071e5d8a76758f777e71b2bde7cdb097
retore <44674058+retore404@users.noreply.github.com>
2020-08-03 12:53:46 +0900

    update: サインアップ機能の実装

--- logger/views.py
+++ logger/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.template import loader
+from .forms import *
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Index dummy')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('logger:index')
+    else:
+        form = SignUpForm()
+
+    context = {'form':form}
+    return render(request, 'logger/signup.html', context)
