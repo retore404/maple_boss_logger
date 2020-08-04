98cf8765a1ff79231d09606e4741c10e76461fed
retore <44674058+retore404@users.noreply.github.com>
2020-08-04 12:39:36 +0900

    update: ボス挑戦履歴の登録機能

--- logger/views.py
+++ logger/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from .forms import *
+from .models import UserBossHistory, Boss
 
 def index(request):
     return HttpResponse('Index dummy')
@@ -23,7 +24,19 @@ def register(request):
     if request.method == 'POST':
         form = BossRegisterForm(request.POST)
         if form.is_valid():
-            boss_id = form.cleaned_data.get('boss_id').boss_id
+            # 入力値
+            user = request.user # 操作中のユーザ情報
+            boss_id = form.cleaned_data.get('boss_id')
+            datetime = form.cleaned_data.get('datetime')
+            
+            # 入力値から対象のBossレコードを特定
+            boss = Boss.objects.filter(boss_id=boss_id)[0]
+
+            # データを更新（不存在であれば追加）
+            obj, created = UserBossHistory.objects.update_or_create(
+                user_id=user, boss_id=boss,
+                defaults={'last_challenged_date': datetime}
+            )
             return redirect('logger:index')
     else:
         form = BossRegisterForm()
