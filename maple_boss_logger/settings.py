04133b40d19acf3b09ff5d69a09f6ad982dd9238
retore <44674058+retore404@users.noreply.github.com>
2020-08-08 16:40:57 +0900

    modify: 取得する環境変数の変更

--- maple_boss_logger/settings.py
+++ maple_boss_logger/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 }
 
 # Github Actions自動テスト用の設定
-if os.environ.get('GITHUB_WORKFLOW')=='test':
+if os.environ.get('GITHUB_ACTIONS'):
     DATABASE = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
